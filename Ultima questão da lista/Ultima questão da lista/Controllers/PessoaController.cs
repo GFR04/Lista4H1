468876c9040e40d311a9c1949ec0d76eefb6a8e4
GFR04 <145131047+GFR04@users.noreply.github.com>
2024-08-28 09:38:13 -0300

    Add files via upload

---
+++ Ultima questão da lista/Ultima questão da lista/Controllers/PessoaController.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using Ultima_questão_da_lista.Models;
+
+namespace Ultima_questão_da_lista.Controllers
+{
+    [ApiController]
+    [Route("API/Pessoa")]
+    public class PessoaController : ControllerBase
+    {
+        static List<Pessoa> pessoaList;
+        [HttpPost]
+        [Route("CriarPessoa")]
+        public IActionResult CriarPessoa(Pessoa _pessoa)
+        {
+            pessoaList.Add (_pessoa);
+            return Ok();
+        }
+        [HttpPut]
+        [Route("AtualizarPessoa")]
+        public IActionResult AtualizarPessoa(string cpf, Pessoa AttPessoa)
+        {
+           Pessoa? PessoaAtualizar = pessoaList.Where(a => a.CPF == cpf).FirstOrDefault();
+            if (PessoaAtualizar == null)
+            {
+                return BadRequest("Essa pessoa não foi encontrada");
+            }
+            PessoaAtualizar.Nome = AttPessoa.Nome;
+            PessoaAtualizar.Peso = AttPessoa.Peso;
+            PessoaAtualizar.Altura = AttPessoa.Altura;
+            return Ok("Os dados de cadastro foram atualizados");
+    }
+        [HttpDelete]
+        [Route("DeletarPessoa")]
+        public IActionResult DeletarPessoa(string cpf)
+        {
+            Pessoa? PessoaRemover = pessoaList.Where(a => a.CPF == cpf).FirstOrDefault();
+
+            if (PessoaRemover == null) 
+            {
+                return BadRequest("Pessoa não encontrada");
+            }
+            pessoaList.Remove(PessoaRemover);
+            return Ok("Pessoa Removida");
+        }
+        [HttpGet]
+        [Route("ListarTodos")]
+        public IActionResult ListarTodos()
+        {
+            return Ok(pessoaList);
+        }
+        [HttpGet]
+        [Route("BuscarCPF")]
+        public IActionResult BuscarCPF(string cpf)
+        {
+            Pessoa? BuscarPessoa = pessoaList.Where(a => a.CPF == cpf).FirstOrDefault();
+            if (BuscarPessoa is null)
+            {
+                return BadRequest("Pessoa não encontrada");
+            }
+            return Ok(BuscarPessoa);
+        }
+        [HttpGet]
+        [Route("IMCbom")]
+        public IActionResult IMCbom ()
+        {
+            return Ok( pessoaList.Where(pessoa => pessoa.IMC() >= 18.5 && pessoa.IMC() <= 24.9));
+
+        }
+        [HttpGet]
+        [Route("SearchPersonByName/{name}")]
+        public IActionResult SearchPersonByName(string nome)
+        {
+            Pessoa? personFound = pessoaList.Where(a => a.Nome == nome).FirstOrDefault();
+
+            if (personFound is null)
+            {
+                return BadRequest("This person not exist!");
+            }
+
+            return Ok(personFound);
+        }
+    }
+}
---
+++ Ultima questão da lista/Ultima questão da lista/Models/Pessoa.cs
@@ -0,0 +1,14 @@
+﻿namespace Ultima_questão_da_lista.Models
+{
+    public class Pessoa
+    {
+        public string Nome { get; set; }
+        public string CPF { get; set; }
+        public int Peso { get; set; }
+        public double Altura { get; set; }
+    public double IMC()
+    {
+        return (Peso / (Altura * Altura));
+    }
+    }
+}
